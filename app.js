9e443efedc57245f5bd00d639956a337c9dade7c
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-17 01:09:05 +0530

    import  userRoutes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import customerRouter from "./routes/customerRoutes.js";
 import itemRouter from "./routes/itemRoutes.js";
 import saleRouter from "./routes/saleRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 import connectDB from "./config/db.js";
 
 const app = express();
@@ -14,6 +15,7 @@ connectDB();
 app.use("/api/customers", customerRouter);
 app.use("/api/items", itemRouter);
 app.use("/api/sales", saleRouter);
+app.use("/api/users", userRouter);
 
 app.listen(PORT, () =>
   console.log(`Server is running in http://localhost:${PORT}`)
