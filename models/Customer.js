d6950957027961b1cd32b7b5c96bda0a17497d89
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-16 22:39:01 +0530

    customer schema created

--- models/Customer.js
+++ models/Customer.js
@@ -1,1 +1,45 @@
 import mongoose from "mongoose";
+
+const customerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+    },
+
+    address: {
+      type: String,
+      optional: null,
+      trim: true,
+    },
+
+    notes: {
+      type: String,
+      optional: null,
+      trim: true,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: false,
+    versionKey: false,
+  }
+);
+
+export default mongoose.model("Customer", customerSchema);
