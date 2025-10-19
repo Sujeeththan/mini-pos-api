8778a6f7db2704d4037011ae11932bf74c1634b4
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-19 20:09:05 +0530

    versionKey add

--- models/User.js
+++ models/User.js
@@ -29,6 +29,11 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+},
+  {
+    timestamps: false,
+    versionKey: false,
+    
 });
 
 export default mongoose.model("User", userSchema);
